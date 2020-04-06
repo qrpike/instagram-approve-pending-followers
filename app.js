156f69fab4077b4d52b366f7054291bbd1499f42
Quinton Pike <qrpike@gmail.com>
2020-04-05 23:23:27 -0400

    Update to use latest library versions

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-const lodash = require('lodash')
-const Promise = require('bluebird')
-var Client = require('instagram-private-api').V1
+const lodash 		= require('lodash')
+const Promise 		= require('bluebird')
+const IgApiClient 	= require('instagram-private-api').IgApiClient
+const ig 			= new IgApiClient()
 
 var program = require('commander')
 let session = null
@@ -20,35 +21,61 @@ if( lodash.isEmpty( program.username ) || lodash.isEmpty( program.password ) ){
 	throw(new Error('Must supply -u {username} and -p {password}'))
 }
 
-var device = new Client.Device(program.username)
-var storage = new Client.CookieFileStorage(`./cookie-${program.username}.json`)
+ig.state.generateDevice(program.username)
 
 let pendingCount = 0
-function DoApprovals(){
-	return Client.Relationship.pendingFollowers( session )
-		.then(( pendingFollowers ) => {
-			console.log('Pending Count:', pendingFollowers.length)
-			pendingCount = pendingFollowers.length
-			return Promise.mapSeries( pendingFollowers, ( pending ) => {
-				console.log('Approving:', pending._params.username)
-				return Promise.delay(1500).then( pending.approvePending.bind( pending ) )
-			}).then(() => {
-				if( pendingCount != 0 ){
-					console.log('Approvals done, still more so continue until pending = 0')
-					return DoApprovals()
-				}
-				setTimeoutContinue()
-			})
-		}).catch(( err ) => {
-			console.log('Do approvals error:', err)
-			console.log('Waiting 11sec before starting again...')
-			return Promise.delay(11500).then(() => {
-				console.log('Starting again...')
-				DoApprovals()
-			})
+async function DoApprovals(){
+
+	const pendFriend = ig.feed.pendingFriendships(session.pk)
+	const pendFriendItems = await pendFriend.items()
+	  
+	console.log('Awaiting Approval: ', pendFriendItems.length)
+
+	return Promise.mapSeries(pendFriendItems, async ( friend ) => {
+		console.log('Approving: ', friend.username)
+		const body = await ig.friendship.approve(friend.pk)
+		return Promise.delay( randomIntFromInterval(350, 1450) )
+	}).then(() => {
+		if( pendFriendItems.length != 0 ){
+			return DoApprovals()
+		}
+		setTimeoutContinue()
+	}).catch(( err ) => {
+		console.log('Do approvals error:', err)
+		console.log('Waiting 11sec before starting again...')
+		return Promise.delay(11500).then(() => {
+			console.log('Starting again...')
+			DoApprovals()
 		})
+	})
+
+	// return Client.Relationship.pendingFollowers( session )
+	// 	.then(( pendingFollowers ) => {
+	// 		console.log('Pending Count:', pendingFollowers.length)
+	// 		pendingCount = pendingFollowers.length
+	// 		return Promise.mapSeries( pendingFollowers, ( pending ) => {
+	// 			console.log('Approving:', pending._params.username)
+	// 			return Promise.delay(1500).then( pending.approvePending.bind( pending ) )
+	// 		}).then(() => {
+	// 			if( pendingCount != 0 ){
+	// 				console.log('Approvals done, still more so continue until pending = 0')
+	// 				return DoApprovals()
+	// 			}
+	// 			setTimeoutContinue()
+	// 		})
+	// 	}).catch(( err ) => {
+	// 		console.log('Do approvals error:', err)
+	// 		console.log('Waiting 11sec before starting again...')
+	// 		return Promise.delay(11500).then(() => {
+	// 			console.log('Starting again...')
+	// 			DoApprovals()
+	// 		})
+	// 	})
 }
 
+function randomIntFromInterval(min, max) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function setTimeoutContinue(){
 	clearTimeout( intervalFunc )
@@ -61,16 +88,25 @@ function setTimeoutContinue(){
 	}, 1000 * 60 * everyMinutes )
 }
 
-// Login and go
-Client.Session.create(device, storage, program.username, program.password)
-	.then(( ses ) => {
-		session = ses
-		DoApprovals().catch(( err ) => {
-			console.log('Create sessions error:', err)
-			setTimeoutContinue()
-		})
-	})
-	.catch(( err ) => {
-		console.log('err:', err)
-	})
 
+// Login and go
+async function main(){
+
+	await ig.simulate.preLoginFlow()
+
+	ig.account.login(program.username, program.password)
+		.then(( ses ) => {
+			console.log(`Logged In! ${ses.username} - ${ses.full_name}`)
+			process.nextTick(async () => await ig.simulate.postLoginFlow())
+			session = ses
+			DoApprovals().catch(( err ) => {
+				console.log('Create sessions error:', err)
+				setTimeoutContinue()
+			})
+		})
+		.catch(( err ) => {
+			console.log('err:', err)
+		})
+}
+
+main()
